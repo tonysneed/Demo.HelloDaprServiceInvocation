fa2517ff2ecca93fe5f24468252c8ac5c8f5aef0
Sneed, Anthony <anthony.sneed@hilti.com>
2023-12-19 15:55:50 -0600

    Initial commit

---
+++ HelloDaprServiceInvocation.Console/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+using Dapr.Client;
+
+Console.WriteLine("Invoking Web API ...");
+
+using var client = new DaprClientBuilder().Build();
+
+var weatherForecasts =
+    await client.InvokeMethodAsync<List<WeatherForecast>>(
+        HttpMethod.Get, "api", "weatherforecast");
+
+foreach (var weatherForecast in weatherForecasts)
+{
+    Console.WriteLine(
+        $"Date:{weatherForecast.Date}, TemperatureC:{weatherForecast.TemperatureC}, Summary:{weatherForecast.Summary}");
+}
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
